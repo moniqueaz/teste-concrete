bbe59eec619199d2b5d1de81edfb20b1efb77036
Monique Azevedo <monique.silva@maxmilhas.com.br>
2020-04-16 11:30:58 -0300

    Adiciona Storybook ao projeto

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ['../src/**/*.stories.(js|mdx)'],
+  addons: [
+    '@storybook/addon-docs',
+    '@storybook/preset-create-react-app',
+    '@storybook/addon-actions',
+    '@storybook/addon-links',
+  ],
+};
