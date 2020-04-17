29484a2de7d1fa579ec7f09498f475b02f015da2
“Monique <moniqueaz.si@gmail.com>
2020-04-17 20:43:06 -0300

    Finaliza component Title

---
+++ src/components/atoms/Title/styles.js
@@ -0,0 +1,9 @@
+import styled, { keyframes, css } from 'styled-components';
+
+export const TitleContainer = styled.div`
+  * {
+    font-size: 35px;
+    font-weight: 500;
+    color: ${props => props.color};
+  }
+`;
