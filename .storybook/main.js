d38bb8d7be911e130c956b93e00513786e8d4f99
“Monique <moniqueaz.si@gmail.com>
2020-04-17 15:31:03 -0300

    Adicionar viewport no storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,5 +5,6 @@ module.exports = {
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
     '@storybook/addon-links',
+    '@storybook/addon-viewport/register',
   ],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,54 @@
 import React from 'react';
-import { addDecorator } from '@storybook/react';
+import { addDecorator, addParameters } from '@storybook/react';
 import GlobalStyle from '../src/styles/global';
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+
+const newViewports = {
+  mobile: {
+    name: 'Mobile',
+    styles: {
+      width: '320px',
+      height: '568px',
+    },
+  },
+  tablet: {
+    name: 'Tablet',
+    styles: {
+      width: '768px',
+      height: '1024px',
+    },
+  },
+  tabletLarge: {
+    name: 'Tablet Large',
+    styles: {
+      width: '1024px',
+      height: '800px',
+    },
+  },
+  Desktop: {
+    name: 'Desktop',
+    styles: {
+      width: '1360px',
+      height: '1024pxpx',
+    },
+  },
+};
+
+// configureViewport({
+//   viewports: newViewports,
+// });
 
 const styles = {
   padding: '20px',
 };
 const Container = ({ children }) => <div style={styles}>{children}</div>;
+
+addParameters({
+  viewport: {
+    viewports: newViewports, // newViewports would be an ViewportMap. (see below for examples)
+    defaultViewport: 'someDefault',
+  },
+});
 
 addDecorator(story => (
   <>
