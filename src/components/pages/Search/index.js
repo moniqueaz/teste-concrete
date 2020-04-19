67bcb5fe4f27e0485a14b552e766721fbbbc36bf
“Monique <moniqueaz.si@gmail.com>
2020-04-19 11:01:13 -0300

    Finaliza pagina serach

--- src/components/pages/Search/index.js
+++ src/components/pages/Search/index.js
@@ -2,28 +2,30 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Logo from '../../atoms/Logo';
 import SearchForm from '../../molecules/Search';
-import Default from '../../templates/Default';
 
 import { Container } from './styles';
 
 const Search = () => {
+  const [value, setValue] = useState('');
   const location = useHistory();
 
   const redirect = name => {
     location.push(`/user/${encodeURIComponent(name)}`);
   };
 
-  const handleSearch = name => {
-    redirect(name);
+  const handleSearch = input => {
+    setValue(input);
   };
 
+  useEffect(() => {
+    value && redirect(value);
+  }, [value]);
+
   return (
-    <Default>
-      <Container className="search">
-        <Logo size="large" />
-        <SearchForm onSearch={handleSearch} />
-      </Container>
-    </Default>
+    <Container className="search">
+      <Logo size="large" />
+      <SearchForm onSearch={handleSearch} />
+    </Container>
   );
 };
 
